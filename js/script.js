b30eeb005a065225478823e1b76771083b569bff
Jeziorna Ula <ulajeziorna.de@gmail.com>
2021-12-12 20:20:15 +0100

    Zaprogramowanie całego zdarzenia wywołanego na elemencie button. Generowanie liczb, sortowanie i wyswietlanie kolumn z liczbami na stronie, końcowe ostylowanie projektu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const generateRandomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
 const createElement = (elType, elAttrs, elProps) => {
   const newEl = document.createElement(elType)
 
@@ -33,3 +37,111 @@ let btnGenerateNumbers = createElement(
 )
 document.body.prepend(btnGenerateNumbers)
 // console.log(btnGenerateNumbers);
+
+
+btnGenerateNumbers.addEventListener('click', () => {
+  let arrEvenNumbers = []
+  let arrOddNumbers = []
+
+
+  // generate 20 random numbers
+  for (let i = 0; i < 20; i++) {
+    const randomNum = generateRandomNum(1, 100)
+    // console.log(randomNum);
+
+    // Sprawdzanie czy liczba jest parzysta
+    const even = (randomNum % 2 === 0)
+
+    if (even) {
+      arrEvenNumbers.push(randomNum)
+    } else {
+      arrOddNumbers.push(randomNum)
+    }
+  }
+
+  // console.log(arrEvenNumbers);
+  // console.log(arrOddNumbers);
+
+
+  // sort arrays
+  arrEvenNumbers.sort((a, b) => {
+    return a - b
+  })
+  arrOddNumbers.sort((a, b) => {
+    return a - b
+  })
+
+  // console.log(arrEvenNumbers);
+  // console.log(arrOddNumbers);
+
+
+  // check if divEvenNumbers && divOddNumbers exist and remove them if so ;)
+  let divEvenNumbers = document.querySelector('#evenNumbersContainer')
+  if (divEvenNumbers) {
+    divEvenNumbers.remove()
+  }
+
+  let divOddNumbers = document.querySelector('#oddNumbersContainer')
+  if (divOddNumbers) {
+    divOddNumbers.remove()
+  }
+
+
+  // create even numbers container & list
+  divEvenNumbers = createElement(
+    'div',
+    { class: 'evenNumbersContainer', id: 'evenNumbersContainer' },
+    { innerHTML: '<span>Your Even Numbers!</span>' }
+  )
+  mainContainer.appendChild(divEvenNumbers)
+  const ulEvenNumbers = createElement(
+    'ul',
+    { class: 'evenNumbersList', id: 'evenNumbersList' },
+    {}
+  )
+  divEvenNumbers.appendChild(ulEvenNumbers)
+  // console.log(divEvenNumbers);
+
+  // create odd numbers container
+  divOddNumbers = createElement(
+    'div',
+    { class: 'oddNumbersContainer', id: 'oddNumbersContainer' },
+    { innerHTML: '<span>Your Odd Numbers!</span>' }
+  )
+  mainContainer.appendChild(divOddNumbers)
+
+
+
+  const ulOddNumbers = createElement(
+    'ul',
+    { class: 'oddNumbersList', id: 'oddNumbersList' },
+    {}
+  )
+  divOddNumbers.appendChild(ulOddNumbers)
+  // console.log(divOddNumbers);
+
+  // create elements of even numbers and add it to dom
+  for (const num of arrEvenNumbers) {
+    const liElement = createElement(
+      'li',
+      { class: 'item-even-number' },
+      { innerText: num }
+    )
+    ulEvenNumbers.appendChild(liElement)
+
+  }
+
+  // create elements of odd numbers and add it to dom
+  for (const num of arrOddNumbers) {
+    const liElement = createElement(
+      'li',
+      { class: 'item-odd-number' },
+      { innerText: num }
+    )
+    ulOddNumbers.appendChild(liElement)
+  }
+
+  // console.log(divEvenNumbers);
+  // console.log(divOddNumbers);
+
+})
